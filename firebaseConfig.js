b70f0497cc00b92c73b07e0a0d4630adf6348b02
Wells <danielndobe850@gmail.com>
2025-02-12 15:36:14 +0200

    Removed API key from repo

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { GOOGLE_API_KEY } from "@env"; // Import from @env
 
 const firebaseConfig = {
-  apiKey: GOOGLE_API_KEY, // Use the imported API key
-  authDomain: "carepoint-cf55e.firebaseapp.com",
-  projectId: "carepoint-cf55e",
-  storageBucket: "carepoint-cf55e.firebasestorage.app",
-  messagingSenderId: "296085325126",
-  appId: "1:296085325126:web:8022b10feb890a395f1147",
-  measurementId: "G-1Z3GY607LQ",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { app, db, firebaseConfig };
-// Now the GOOGLE_API_KEY is imported from the .env file and used in the firebaseConfig.js file.
+export { app, db };
